e390d9d5276f3d50d1ce1ccefee43e5d94cedb09
Vishal Garg <vishugarg10004@gmail.com>
2020-10-26 13:20:16 +0530

    [Vishal] created method to insert values in binary search tree and print it.

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    /// <summary>
+    /// Generic Binary search tree is defined
+    /// Due to generic class, Icomparable method is defined to use CompareTo inside the program
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class BinarySearchTree<T> where T : IComparable
+    {
+        //creating a object of Node class, called root which will save the first data coming from the user
+        public Node<T> root;
+
+        /// <summary>
+        /// Inserting data in Binary Search Tree
+        /// </summary>
+        /// <param name="data">data to be entered in binary search tree</param>
+        public void InsertDataInBST(T data) 
+        {
+            //Creating and instatiating node with data in constructor
+            //now we have to point node to correct addresss so that condition of Binary search tree is satisfied
+            Node<T> node = new Node<T>(data);
+
+            //when there is no initial data and root is null, first value is added in root.
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                //current position node is defined which have the value of root element.
+                Node<T> currentPosition = root;
+                //loop is instatiated to add value in binary search tree
+                while (true)
+                {
+                    //current position is assigned to parent node
+                    //this is done because parent node will contain the data of currentposition, where as current position will be changed
+                    //to point to the left or right node 
+                    //hence parent node with value of currentPosition(root) will be pointing to left or right to add a data in it.
+                    Node<T> parent = currentPosition;
+                    //when data is less than the data in root, currentPosition node will point to left direction
+                    if (currentPosition.data.CompareTo(data) > 0)
+                    {
+                        currentPosition = currentPosition.left;
+                        //if there is no node in it and value is null, as current position is now null
+                        //parent node is used to point to left direction and add  node with data.
+                        if (currentPosition == null)
+                        {
+                            parent.left = node;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        //when data is greater than right position, than root node will point to right direction
+                        //address of right node is passed in current position and currentPosition is changed
+                        //if right node is null, then parent node is used to assign value at right.
+                        currentPosition = currentPosition.right;
+                        if (currentPosition == null)
+                        {
+                            parent.right = node;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Displaying the data in binary search tree in sorted manner
+        /// for displaying the data in sorted manner, printing should start from leftmost leaf node.
+        /// </summary>
+        public void DisplayDatainBST()
+        {
+            //parent node is defined and equalized to root, which helps in printing values when coming out of recursion in function
+            Node<T> parent = root;
+            //if pointer to left node is not null, then root is changed to left value, and parent node still represents root.
+            //displaydatainBST is again called and root is changed to represent left node.
+            //this recursion continues until left most node becomes null and value is printed for that particular parent node.
+            if (root.left!=null)
+            {
+                parent = root;
+                root = root.left;
+                DisplayDatainBST();
+            }
+            Console.WriteLine("Data in sorted manner from :\t" + parent.data);
+            //after printing first value for which left node is null
+            //it's parent node is checked to have right node, if right node is there, then root points to right node and again recursion is done
+            //recursion again finds the leftmost null value and displays the node and process is repeated
+            //if pointer has moved from leftmost to rightmost value, then process begins to come out of recursion
+            //in that case for each value coming out of recursion, it's parent node is printed using parent node assigned value in if condition 
+            //and checked for right node
+            //for better understanding, do debugging again
+            if(root.right!=null)
+            {
+                root = root.right;
+                DisplayDatainBST();
+            }
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,16 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Binary Search Tree");
+
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+            binarySearchTree.InsertDataInBST(23);
+            binarySearchTree.InsertDataInBST(344);
+            binarySearchTree.InsertDataInBST(43);
+            binarySearchTree.InsertDataInBST(266);
+            binarySearchTree.InsertDataInBST(232);
+
+            binarySearchTree.DisplayDatainBST();
         }
     }
 }
